66a1930227cbd2fac0a5e350ac9cd98ea4691757
Fraser Murray <fraser.m.murray@gmail.com>
2015-04-16 09:18:46 +0100

    remove rogue IndexedList stuff

--- test/Network/API/Builder/QuerySpec.hs
+++ test/Network/API/Builder/QuerySpec.hs
@@ -26,12 +26,6 @@ spec =
       toQuery "maybe" (Just False) `shouldBe` [("maybe", "false")]
       toQuery "maybe" (Nothing :: Maybe Bool) `shouldBe` []
 
-    it "can convert indexed lists" $ do
-      toQuery "list" (IndexedList [True, False, False]) `shouldBe` [("list[0]", "true"), ("list[1]", "false"), ("list[2]", "false")]
-      skip "broken instance" $
-        toQuery "nested" (IndexedList (map IndexedList [[True], [False, False], [True]]))
-          `shouldBe` [("nested[0][0]", "true"), ("nested[1][0]", "false"), ("nested[1][1]", "false"), ("nested[2][0]", "true")]
-
     prop "toQuery x y == toQuery x (Just y)" $ \(y :: Int) ->
       toQuery "x" y == toQuery "x" (Just y)
 
