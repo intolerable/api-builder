8e4cb7f3a6451e42f6b539bacae7a0d3ad35e0d6
Fraser Murray <fraser.m.murray@gmail.com>
2015-07-25 00:18:39 +0100

    renames

--- src/Network/API/Builder/API.hs
+++ src/Network/API/Builder/API.hs
@@ -2,6 +2,7 @@ module Network.API.Builder.API (
   -- * API
     API
   , APIT
+  , ResumeAPIT
   -- ** Running the API
   , execAPI
   , runAPI
@@ -43,47 +44,49 @@ import Network.HTTP.Client.TLS
 
 -- | Main API type. @s@ is the API's internal state, @e@ is the API's custom error type,
 --   and @a@ is the result when the API runs. Based on the @APIT@ transformer.
-type API s e a = APIT s e a IO a
+type API s e a = APIT s e IO a
+
+type APIT s e m a = ResumeAPIT s e a m a
 
 -- | Main API transformer type. @s@ is the API's internal state, @e@ is the API's custom error type,
 --   and @a@ is the result when the API runs.
-newtype APIT s e r m a =
-  APIT (ContT r (ExceptT (APIError e, APIT s e r m r) (ReaderT Manager (StateT Builder (StateT s m)))) a)
+newtype ResumeAPIT s e r m a =
+  ResumeAPIT (ContT r (ExceptT (APIError e, ResumeAPIT s e r m r) (ReaderT Manager (StateT Builder (StateT s m)))) a)
   deriving (Functor, Applicative, Monad, MonadIO)
 
-instance MonadTrans (APIT s e r) where
-  lift = APIT . lift . lift . lift . lift . lift
+instance MonadTrans (ResumeAPIT s e r) where
+  lift = ResumeAPIT . lift . lift . lift . lift . lift
 
 -- | Lifts an action that works on an @API@ to an action that works on an @API@.
 --   This function is provided solely for future-proofing in the case that more transformers
 --   need to be stacked on top - it's implemented simply as @id@ for the moment.
-liftExcept :: Monad m => ExceptT (APIError e, APIT s e r m r) (ReaderT Manager (StateT Builder (StateT s m))) a -> APIT s e r m a
-liftExcept = APIT . lift
+liftExcept :: Monad m => ExceptT (APIError e, ResumeAPIT s e r m r) (ReaderT Manager (StateT Builder (StateT s m))) a -> ResumeAPIT s e r m a
+liftExcept = ResumeAPIT . lift
 
 {-# DEPRECATED liftEither "Use liftExcept" #-}
 -- | Identical to 'liftExcept', provided for (almost) compatibility.
-liftEither :: Monad m => ExceptT (APIError e, APIT s e r m r) (ReaderT Manager (StateT Builder (StateT s m))) a -> APIT s e r m a
-liftEither = APIT . lift
+liftEither :: Monad m => ExceptT (APIError e, ResumeAPIT s e r m r) (ReaderT Manager (StateT Builder (StateT s m))) a -> ResumeAPIT s e r m a
+liftEither = ResumeAPIT . lift
 
 -- | Lifts an action that works on a @Manager@ to one that works on an @API@.
-liftManager :: Monad m => ReaderT Manager (StateT Builder (StateT s m)) a -> APIT s e r m a
-liftManager = APIT . lift . lift
+liftManager :: Monad m => ReaderT Manager (StateT Builder (StateT s m)) a -> ResumeAPIT s e r m a
+liftManager = ResumeAPIT . lift . lift
 
 -- | Lifts an action that operates on a @Builder@ to one that works on an @API@. Useful
 --   mainly for gaining access to a @Builder@ from inside an @API@.
-liftBuilder :: Monad m => StateT Builder (StateT s m) a -> APIT s e r m a
-liftBuilder = APIT . lift . lift . lift
+liftBuilder :: Monad m => StateT Builder (StateT s m) a -> ResumeAPIT s e r m a
+liftBuilder = ResumeAPIT . lift . lift . lift
 
 -- | Lifts an action on an @API@'s state type @s@ to one that works on the @API@. Good
 --   for messing with the state from inside the @API@.
-liftState :: Monad m => StateT s m a -> APIT s e r m a
-liftState = APIT . lift . lift . lift . lift
+liftState :: Monad m => StateT s m a -> ResumeAPIT s e r m a
+liftState = ResumeAPIT . lift . lift . lift . lift
 
 -- | Runs an @API@ by executing its transformer stack and dumping it all into @IO@. Only returns the actual result.
 execAPI :: MonadIO m
        => Builder -- ^ initial @Builder@ for the @API@
        -> s -- ^ initial state @s@ for the @API@
-       -> APIT s e a m a -- ^ the actual @API@ to run
+       -> ResumeAPIT s e a m a -- ^ the actual @API@ to run
        -> m (Either (APIError e) a) -- ^ IO action that returns either an error or the result
 execAPI b s api = do
   m <- liftIO $ newManager tlsManagerSettings
@@ -97,9 +100,9 @@ runAPI :: MonadIO m
        => Builder -- ^ initial @Builder@ for the @API@
        -> Manager -- ^ manager for working with conduit functions
        -> s -- ^ initial state @s@ for the @API@
-       -> APIT s e a m a -- ^ the actual @API@ to run
+       -> ResumeAPIT s e a m a -- ^ the actual @API@ to run
        -> m (Either (APIError e) a, Builder, s) -- ^ IO action that returns either an error or the result, as well as the final states
-runAPI b m s (APIT api) = do
+runAPI b m s (ResumeAPIT api) = do
   ((res, b'), s') <- runStateT (runStateT (runReaderT (runExceptT (runContT api return)) m) b) s
   return (bimap fst id res, b', s')
 
@@ -107,20 +110,20 @@ runResumeAPI :: MonadIO m
              => Builder
              -> Manager
              -> s
-             -> APIT s e a m a
-             -> m (Either (APIError e, APIT s e a m a) a, Builder, s)
-runResumeAPI b m s (APIT api) = do
+             -> ResumeAPIT s e a m a
+             -> m (Either (APIError e, ResumeAPIT s e a m a) a, Builder, s)
+runResumeAPI b m s (ResumeAPIT api) = do
   ((res, b'), s') <- runStateT (runStateT (runReaderT (runExceptT (runContT api return)) m) b) s
   return (res, b', s')
 
 -- | Runs a @Route@. Infers the type to convert to from the JSON with the @a@ in @API@,
 --   and infers the error type from @e@.
-runRoute :: (Receivable a, ErrorReceivable e, MonadIO m) => Route -> APIT s e r m a
+runRoute :: (Receivable a, ErrorReceivable e, MonadIO m) => Route -> ResumeAPIT s e r m a
 runRoute = sendRoute ()
 
 -- | Runs a @Route@, but only returns the response and does nothing towards
 --   decoding the response.
-routeResponse :: (MonadIO m, ErrorReceivable e) => Route -> APIT s e r m (Response ByteString)
+routeResponse :: (MonadIO m, ErrorReceivable e) => Route -> ResumeAPIT s e r m (Response ByteString)
 routeResponse = sendRoute ()
 
 eitherOr :: Maybe a -> b -> Either b a
@@ -129,12 +132,12 @@ a `eitherOr` b =
     Just x -> Right x
     Nothing -> Left b
 
-sendRoute :: (MonadIO m, Sendable t, ErrorReceivable e, Receivable a) => t -> Route -> APIT s e r m a
+sendRoute :: (MonadIO m, Sendable t, ErrorReceivable e, Receivable a) => t -> Route -> ResumeAPIT s e r m a
 sendRoute s r = do
   builder <- liftBuilder get
   manager <- liftManager ask
-  APIT $ do
-    label <- fmap APIT $ mkLabel
+  ResumeAPIT $ do
+    label <- fmap ResumeAPIT $ mkLabel
     lift $ mapExceptT (fmap $ addLabel label) $ do
       req <- ExceptT $ return $ send builder r s `eitherOr` InvalidURLError
       response <- liftIO $ try $ httpLbs req manager
@@ -155,20 +158,20 @@ routeRequest b route = send b route ()
 
 -- | Modify the @name@ of the @Builder@ from inside an API. Using this is probably not the best idea,
 --   it's nice if the @Builder@'s name is stable at least.
-name :: Monad m => Text -> APIT s e r m ()
+name :: Monad m => Text -> ResumeAPIT s e r m ()
 name t = liftBuilder $ modify (\b -> b { _name = t })
 
 -- | Modify the @baseURL@ of the @Builder@ from inside an API.
 --   Can be useful for changing the API's endpoints for certain requests.
-baseURL :: Monad m => Text -> APIT s e r m ()
+baseURL :: Monad m => Text -> ResumeAPIT s e r m ()
 baseURL t = liftBuilder $ modify (\b -> b { _baseURL = t })
 
 -- | Modify every @Route@ before it runs. Useful for adding extra params to every query,
 --   for example.
-customizeRoute :: Monad m => (Route -> Route) -> APIT s e r m ()
+customizeRoute :: Monad m => (Route -> Route) -> ResumeAPIT s e r m ()
 customizeRoute f = liftBuilder $ modify (\b -> b { _customizeRoute = f })
 
 -- | Modify every @Request@ before the API fetches it. Useful for adding headers to every request,
 --   for example.
-customizeRequest :: Monad m => (Request -> Request) -> APIT s e r m ()
+customizeRequest :: Monad m => (Request -> Request) -> ResumeAPIT s e r m ()
 customizeRequest f = liftBuilder $ modify (\b -> b { _customizeRequest = f })
